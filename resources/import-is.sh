56bae088d85a21a901133c8c1b90fdf0990c21a6
Brian Cook <bcook@redhat.com>
2016-06-03 11:16:20 -0500

    added import DB templates

--- resources/import-is.sh
+++ resources/import-is.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~
+cd ~/deploy-ose
 wget https://raw.githubusercontent.com/openshift/origin/master/examples/image-streams/image-streams-rhel7.json
 
 #Get the builder image imagesteams.
@@ -17,6 +17,10 @@ for f in image-streams-rhel7.json; do cat $f | oc create -n openshift -f -; done
   sleep 1
 done
 
+# get the database tempaltes and install them
+svn export https://github.com/openshift/origin/trunk/examples/db-templates
 
 # import the DB templates
-# for f in db-templates/*.json; do cat $f | oc create -n openshift -f -; done
+for f in db-templates/*.json; do cat $f | oc create -n openshift -f -; done
+
+cd ~
